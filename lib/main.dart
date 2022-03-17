4c2f273fbdc103d468c8b77eb4aef91d03252720
affection7 <salkow12345@gmail.com>
2022-03-17 13:56:03 +0300

    Форматирование

--- lib/main.dart
+++ lib/main.dart
@@ -1,44 +1,39 @@
 import 'package:flutter/material.dart';
 import 'dart:math';
 
+void main() => runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Scaffold(
+        appBar: AppBar(
+            title: Container(
+          child: Text(
+            'Список элементов',
+            style: TextStyle(color: Colors.black),
+          ),
+        )),
+        body: new Center(
+          child: new MyBody(),
+        ))));
+
 class MyBody extends StatefulWidget {
   @override
   createState() => new MyBodyState();
 }
 
 class MyBodyState extends State<MyBody> {
-
   @override
   Widget build(BuildContext context) {
-
     return ListView.builder(itemBuilder: (context, i) {
       print('num $i : нечетное = ${i.isOdd}');
-
-      if (i.isOdd) return new Divider(
-        color: Colors.amber,
-      );
       final int index = i ~/ 2;
+      if (i.isOdd)
+        return new Divider(
+          color: Colors.amber,
+        );
 
       index.toDouble();
-
-      //if (index >= _array.length)
-        //_array.addAll(['$index', '${index + 1}', '${index + 2}']);
-      num x = pow(2,index);
+      num x = pow(2, index);
       return Text('2' '^' '$index' ' ' '=' '$x');
     });
   }
 }
-
-void main() =>
-    runApp(MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: Scaffold(
-          appBar: AppBar(
-            title: Container(
-              child:Text('Список элементов', style: TextStyle(color: Colors.black),),
-
-            )
-          ),
-            body: new Center(
-              child: new MyBody(),
-            ))));
